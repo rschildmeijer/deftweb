e53732fffc787254f203a66b76ea1f6c428d74ec
rschildmeijer <schildmeijer@gmail.com>
2010-10-09 12:32:26 +0200

    added group capturing RH section and Logging section

--- src/org/deftserver/deftweb/client/Deftweb.java
+++ src/org/deftserver/deftweb/client/Deftweb.java
@@ -25,7 +25,7 @@ public class Deftweb implements EntryPoint {
 		vp.setHorizontalAlign(HorizontalAlignment.CENTER);
 		TabPanel panel = new TabPanel();  
 		panel.setPlain(true);  
-		panel.setSize(850, 1550); 
+		panel.setSize(850, 2050); 
 
 		TabItem documentationTab = new DocumentationTab();  
 		documentationTab.addStyleName("pad-text");  
--- src/org/deftserver/deftweb/client/tabs/documentation/DocumentationTab.java
+++ src/org/deftserver/deftweb/client/tabs/documentation/DocumentationTab.java
@@ -43,34 +43,26 @@ public class DocumentationTab extends TabItem {
 	}
 
 	private Widget getInitialHtml() {
-		//		<h2>Download and install</h2>
-		//		  <p><b>Download:</b> <a href="http://github.com/downloads/facebook/tornado/tornado-1.1.tar.gz">tornado-1.1.tar.gz</a></p>
-		//		  <pre><code>tar xvzf tornado-1.1.tar.gz
-		//		cd tornado-1.1
-		//		python setup.py build
-		//		sudo python setup.py install</code></pre>
 		return new HTML(
 				"<br><br><h3>Overview</h3>" +
-				"The Deft web server is an open source projected (licensed under <a href=\"http://www.apache.org/licenses/LICENSE-2.0.html\">Apache version 2</a>). Deft was intitially inspired by <a href=\"http://github.com/facebook/tornado\">facebook/tornado</a>." +
+				"Deft web server is an open source projected (licensed under <a href=\"http://www.apache.org/licenses/LICENSE-2.0.html\">Apache version 2</a>). Deft was intitially inspired by <a href=\"http://github.com/facebook/tornado\">facebook/tornado</a>." +
 				"<br>Deft is a single threaded, asynchronous, event driven high performance web server running on the JVM." +
 				"<br><b>Source and issue tracker:</b> <a href=\"http://github.com/rschildmeijer/deft\">http://github.com/rschildmeijer/deft</a>" + 
-				"<br><br><h3>Features</h3>" +
+				"<br><br><br><h3>Features</h3>" +
 				" <i>* Specialized and optimized for thousands of simultaneous connections.</i> (<a href=\"http://en.wikipedia.org/wiki/C10k_problem\">C10k</a>) (<a href=\"http://blog.urbanairship.com/blog/2010/08/24/c500k-in-action-at-urban-airship/\">C500k</a>)" +
 				"<br> <i>* Using pure Java NIO</i> (<a href=\"http://download.oracle.com/javase/6/docs/api/java/nio/package-summary.html\">java.nio</a> & <a href=\"http://download.oracle.com/javase/6/docs/api/java/nio/channels/package-summary.html\">java.nio.channels</a>)" +
 				"<br> *<i> Asynchronous (nonblocking I/O)</i>" +
-				"<br><br><h3>Getting started</h3>" +
+				"<br><br><br><h3>Getting started</h3>" +
 				"<p><b>Download:</b> <a href=\"http://github.com/downloads/rschildmeijer/deft/deft-0.1.0.tar.gz\">deft-0.1.0.tar.gz</a></p>" +
 				"<br><pre><code>    tar xvzf deft-0.1.0.tar.gz<br>" +
 				"</code></pre><br>" +
 				"<h3>Hello world (synchronous)</h3>" +
 				"<pre><code>" +
-				"    class SynchronousRequestHandler extends RequestHandler {<br>" +
-				//"   <br>" +
-				"         &#064;Override<br> " +
+				"    class SynchronousRequestHandler extends RequestHandler {<br><br>" +
+				"        &#064;Override<br>" +
 				"        public void get(HttpRequest request, HttpResponse response) {<br>" +
                 "            response.write(\"hello world!\");<br>" +
-    			"         }<br>"+
-    			//" <br>" +
+    			"        }<br>"+
 				"    }<br>" +
 				"<br>" +
 				"    public static void main(String[] args) {<br>" +
@@ -82,7 +74,7 @@ public class DocumentationTab extends TabItem {
 				"</pre></code>" +
 				"<br><h3>Hello world (asynchronous)</h3>"+ 
 				"<pre><code>" +
-			    "    class AsynchronousRequestHandler extends RequestHandler {<br>" +
+			    "    class AsynchronousRequestHandler extends RequestHandler {<br><br>" +
 			    "        &#064;Override<br>" +
 			    "        &#064;Asynchronous<br>" +
 			    "        public void get(HttpRequest request, final HttpResponse response) {<br>" +
@@ -103,14 +95,43 @@ public class DocumentationTab extends TabItem {
 			    "request is not finished when the get method returns.<br>" +
 			    "When the asynchronous database client eventually calls the callback (i.e. onSuccess(String result)), <br>" +
 			    "the request is still open, and the response is finally flushed to the client with the call to response.finish()." +
-				"<br><br><br><h3>Comparison</h3>"+
+			    "<br><br><h3>Capturing groups with regular expressions</h3>" +
+			    "<pre><code>" +
+			    "    class CapturingRequestHandler extends RequestHandler {<br><br>" +
+			    "        &#064;Override<br>" +
+			    "        public void get(HttpRequest request, HttpResponse response) {<br>" +
+			    "            response.write(request.getRequestedPath());<br>" +
+			    "        }<br>" + 
+			    "    }<br><br>" +
+			    "    public static void main(String[] args) {<br>" +
+			    "        Map&#060;String, RequestHandler&#062; handlers = new HashMap&#060;String, RequestHandler&#062;();<br>" +
+			    "        handlers.put(\"/\", new AsynchronousRequestHandler());<br>" +
+			    "        HttpServer server = new HttpServer(new Application(handlers));<br>" +
+			    "        server.listen(8080).getIOLoop().start();<br>" +
+			    "    }<br>" +
+			    "</pre></code>" +
+			    "The code above creates a \"dynamic mapping\" to the group capturing request handler (CapturingRequestHandler).<br>" +
+			    "This type of mapping is convenient when creating e.g. RESTful web services where you usually address your <br>" +
+			    "resources with the path segment instead of using get parameters. The mapping above will \"capture\" all <br>" + 
+			    "requests made against urls that start with \"/persons/\" and ends with a (positive) number, e.g. \"/persons/1911\" or \"/persons/42\". <br>" +
+			    "Capturing groups can only be used as the last url path segment like the example above. <br>" +
+			    "It's (currently) not possible to have more than one capturing within one \"dynamic mapping\".<br>" +
+			    "<br><br><h3>Logging</h3>" +
+			    "All logging in Deft is performed by Logback (SLF4J). Deft ships with a default Logback config file, in which all<br>" +
+			    "log-output is written to STDOUT. While this works fine for testing purposes, it is recommended to override this behaviour<br>" +
+			    "for \"real world scenarios\". This is done by creating your own logback config file and then specify the location of it<br>" + 
+			    "with a system property named <i>logback.configurationFile</i>. The value of this property can be a URL, a resource on the class path<br>" +
+			    "or a path to a file external to the application.<br>"+
+			    "Example:<br><br>" +
+			    "<pre></code>    java -Dlogback.configurationFile=/path/to/config.xml com.my.DeftServer</pre></code>"+
+			    "<br><br><br><h3>Comparison</h3>"+
 				"The following benchmark was done using Apache benchmark (man ab) which is an HTTP server benchmarking tool. " +
 				"<br> Tornado 1.1, node.js 0.2.3, nginx 0.8.52, python 2.6.1, java 1.6.0_20 were used." + 
 				"<br>The tests were executed on a 2.66ghz i7 quad core with 4gb running mac OS X 10.6.4" +
 				"<br>The first chart shows the result when running a single fronted/instance of each server, and for the second chart we used nginx as" +
 				"<br>a reverse proxy and loadbalancer, and had four instances of each server running for each test." +
 				"<br>The command executed (for both charts) was:<br><br>" +
-				"<pre><code>    ab -k -c[5, 10, 15, 20, 25] -n80000</pre></code>" +
+				"<pre><code>    ab -k -c[5, 10, 15, 20, 25] -n80000 http://127.0.0.1/</pre></code>" +
 				"<br>"
 		);
 	}
